8853fe1186b59356b100786ea0d12b84168d290d
Lenovo <Lenovo@DESKTOP-2GP8M36>
2016-03-30 23:39:09 +0700

    progres tahap2

--- TUBESPBO/src/tubespbo/TUBESPBO.java
+++ TUBESPBO/src/tubespbo/TUBESPBO.java
@@ -15,7 +15,43 @@ public class TUBESPBO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        
+        Gerbong g1 = new Gerbong("A",10);
+        Gerbong g2 = new Gerbong("B",5);
+        Gerbong g3 = new Gerbong("C",8);
+        
+        Kereta k1 = new Kereta("Lodaya");
+        k1.addGerbong(g1);
+        k1.addGerbong(g2);
+        k1.addGerbong(g3);
+        
+        Kereta k2 = new Kereta("Serayu");
+        k2.addGerbong(g3);
+        
+        Stasiun s1 = new Stasiun("Bandung");
+        Stasiun s2 = new Stasiun("Purwokerto");
+        Stasiun s3 = new Stasiun("Surabaya");
+        
+        Rute BdgPwt = new Rute(s1,s2,k2);
+        
+        System.out.println("Kereta 1");
+        for(int i=0;i<k1.getDaftarGerbong().size();i++){
+            System.out.print(k1.getDaftarGerbong().get(i).getNamaGerbong()+ " ");
+        }
+        System.out.println();
+        System.out.println("Max Kapasitas :" + k1.getKapasitasKereta());
+        
+        System.out.println();
+        System.out.println("Kereta 2");
+        for(int i=0;i<k2.getDaftarGerbong().size();i++){
+            System.out.print(k2.getDaftarGerbong().get(i).getNamaGerbong()+ " ");
+        }
+        System.out.println();
+        System.out.println("Max Kapasitas :" + k2.getKapasitasKereta());
+        
+        System.out.println();
+        System.out.println("Rute Bandung Purwokerto - " + BdgPwt.getKereta().getNamaKereta());
+        System.out.println(BdgPwt.getStasiun1().getNamaStasiun());
+        System.out.println(BdgPwt.getStasiun2().getNamaStasiun());
+    }    
 }
