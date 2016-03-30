457ce664bcb641a112f05773986ebd27735a8938
Lenovo <Lenovo@DESKTOP-2GP8M36>
2016-03-30 23:40:37 +0700

    progres tahap2

--- TUBESPBO/src/tubespbo/Kereta.java
+++ TUBESPBO/src/tubespbo/Kereta.java
@@ -3,37 +3,68 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication10;
+package tubespbo;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author Lenovo
  */
 public class Kereta {
- private daftarGerbong[] dg = new daftarGerbong[10];
+    private ArrayList<Gerbong> daftarGerbong;
+    private String namaKereta;
+    private int kapasitasKereta;
 
+    public Kereta(String nama){
+        this.namaKereta = nama;
+        this.daftarGerbong = new ArrayList();
+        this.kapasitasKereta = 0;
+    }
+    
+    public void addGerbong(Gerbong g){
+        this.daftarGerbong.add(g);
+        this.kapasitasKereta = this.kapasitasKereta + g.getMaxBebanGerbong();
+    }
+
+    public String getNamaKereta() {
+        return namaKereta;
+    }
+
+    public void setNamaKereta(String namaKereta) {
+        this.namaKereta = namaKereta;
+    }
+
+    public ArrayList<Gerbong> getDaftarGerbong() {
+        return daftarGerbong;
+    }
+
+    public int getKapasitasKereta() {
+        return kapasitasKereta;
+    }
+    
     /**
      * @return the dg
      */
-    public daftarGerbong[] getDg() {
-        return dg;
-    }
+//    public daftarGerbong[] getDg() {
+//        return dg;
+//    }
 
     /**
      * @param dg the dg to set
      */
-    public void setDg(daftarGerbong[] dg) {
-        this.dg = dg;
-    }
+//    public void setDg(daftarGerbong[] dg) {
+//        this.dg = dg;
+//    }
 
-    public void addGerbong(Gerbong g){
-        if (daftarGerbong<=10){
-            Gerbong[daftarGerbong] = new Gerbong(namaGerbong);
-            Gerbong++;
-        }
-    }
-    
-    public void removeGerbong(Gerbong g){
-        Gerbong[daftarGerbong]--;
-    }
+//    public void addGerbong(Gerbong g){
+//        if (daftarGerbong<=10){
+//            Gerbong[daftarGerbong] = new Gerbong(namaGerbong);
+//            Gerbong++;
+//        }
+//    }
+//    
+//    public void removeGerbong(Gerbong g){
+//        Gerbong[daftarGerbong]--;
+//    }
 }
