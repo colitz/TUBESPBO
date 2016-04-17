f1fba91787cbb02011ce03e70c8cc1671777a235
Lenovo <Lenovo@DESKTOP-2GP8M36>
2016-04-17 21:46:02 +0700

    consol

---
+++ TUBESPBO/src/tubespbo/DriverTiketKA.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubespbo;
+
+/**
+ *
+ * @author Fanny
+ */
+public class DriverTiketKA {
+    public static void main(String[] args) {
+        AplikasiTiketKA app = new AplikasiTiketKA();
+        app.mainMenu();
+    }
+}
