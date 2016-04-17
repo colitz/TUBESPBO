ee92cceb2366f253ccc903a94cf96d2d30a54e97
Lenovo <Lenovo@DESKTOP-2GP8M36>
2016-04-17 21:49:18 +0700

    consol

--- TUBESPBO/src/tubespbo/Tiket.java
+++ TUBESPBO/src/tubespbo/Tiket.java
@@ -10,14 +10,32 @@ package tubespbo;
  * @author Lenovo
  */
 public class Tiket {
+    private String idTiket;
     private Rute rute;
     private int hargaTiket;
     private int tiketTersedia;
     private int tanggal;
+    private Kereta k;
 
     /**
      * @return the hargaTiket
      */
+    public Tiket(String idTiket,Rute rute, int hargaTiket, int tiketTersedia, int tanggal){
+        this.idTiket=idTiket;
+        this.rute=rute;
+        this.hargaTiket=hargaTiket;
+        this.tiketTersedia=tiketTersedia;
+        this.tanggal=tanggal;
+    }
+
+    public String getIdTiket() {
+        return idTiket;
+    }
+
+    public void setIdTiket(String idTiket) {
+        this.idTiket = idTiket;
+    }
+    
     public int getHargaTiket() {
         return hargaTiket;
     }
